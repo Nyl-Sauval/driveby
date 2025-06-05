0b4bc0f7a67fd5583209196afff7d0e45f145092
Beni TUKALAYENGE <btukalayenge@sarbec.com>
2025-06-05 16:19:59 +0200

    Creation de models et migrations

---
+++ database/migrations/2025_06_05_131309_create_retour_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('retour', function (Blueprint $table) {
+            $table->id('return_id');
+            $table->date('return_date');
+            $table->String('return_status_car',50);
+            $table->decimal('return_mileage',15,3);
+            $table->string('return_default',500)->nullable();
+
+            $table->foreignId('user_id')->constrained('cascade');
+            $table->foreignId('location_id')->constrained('locations')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('garanties');
+
+    }
+};
---
+++ database/migrations/2025_06_05_134655_create_garanties_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('garanties', function (Blueprint $table) {
+            $table->id('guarantee_id');
+            $table->string('guarantee_name', 500);
+            $table->decimal('guarantee_price', 10, 2);
+            $table->string('guarantiee_description', 500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('garanties');
+    }
+};
