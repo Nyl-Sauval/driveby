5576c1fe55b5ba606e9246c2ef758429d867edbe
Emma <graveemma842@gmail.com>
2025-06-26 11:41:36 +0200

    Add images

--- server/database/seeders/CarSeeder.php
+++ server/database/seeders/CarSeeder.php
@@ -16,7 +16,7 @@ class CarSeeder extends Seeder
         $cars = [
             [
                 'car_registration' => 'AB-123-CD',
-                'car_picture' => 'cars/clio_v.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => 'Clio V',
                 'car_fuel' => 'ESSENCE',
                 'car_mileage' => 25300.000,
@@ -27,7 +27,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'EF-456-GH',
-                'car_picture' => 'cars/208_peugeot.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => '208',
                 'car_fuel' => 'ESSENCE',
                 'car_mileage' => 19450.000,
@@ -38,7 +38,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'IJ-789-KL',
-                'car_picture' => 'cars/golf7.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => 'Golf 7',
                 'car_fuel' => 'GAZOLE',
                 'car_mileage' => 67400.000,
@@ -49,7 +49,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'MN-321-OP',
-                'car_picture' => 'cars/model3.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => 'Model 3',
                 'car_fuel' => 'ELECTRIQUE',
                 'car_mileage' => 12100.000,
@@ -60,7 +60,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'QR-654-ST',
-                'car_picture' => 'cars/kangoo.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => 'Kangoo',
                 'car_fuel' => 'GAZOLE',
                 'car_mileage' => 81000.000,
@@ -71,7 +71,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'UV-987-WX',
-                'car_picture' => 'cars/500e.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => '500e',
                 'car_fuel' => 'ELECTRIQUE',
                 'car_mileage' => 6700.000,
@@ -82,7 +82,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'YZ-159-AB',
-                'car_picture' => 'cars/c4_picasso.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => 'C4 Picasso',
                 'car_fuel' => 'GAZOLE',
                 'car_mileage' => 93400.000,
@@ -93,7 +93,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'CD-753-EF',
-                'car_picture' => 'cars/qashqai.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => 'Qashqai',
                 'car_fuel' => 'ESSENCE',
                 'car_mileage' => 36500.000,
@@ -104,7 +104,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'GH-852-IJ',
-                'car_picture' => 'cars/audi_a4.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => 'A4',
                 'car_fuel' => 'ESSENCE',
                 'car_mileage' => 52300.000,
@@ -115,7 +115,7 @@ class CarSeeder extends Seeder
             ],
             [
                 'car_registration' => 'KL-147-MN',
-                'car_picture' => 'cars/scenic.jpg',
+                'car_picture' => 'https://mondaycar.com/wp-content/uploads/2022/12/renault-clio-v-1.webp',
                 'car_model' => 'Scénic',
                 'car_fuel' => 'GAZOLE',
                 'car_mileage' => 78400.000,
