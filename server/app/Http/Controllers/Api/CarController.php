b765f5698410b4421eb1a5fe3112053efdf72da0
nyl_sauval <nyl_sauval@ens.univ-artois.fr>
2025-06-27 08:53:30 +0200

    ref #10; Route api détail Voiture

--- server/app/Http/Controllers/Api/CarController.php
+++ server/app/Http/Controllers/Api/CarController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CarCollection;
+use App\Http\Resources\CarResource;
 use App\Models\Car;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class CarController extends Controller
 {
@@ -16,4 +15,9 @@ class CarController extends Controller
         return new CarCollection($cars);
     }
 
+    public function show($carId)
+    {
+        $car = Car::with(['agency', 'categories'])->findOrFail($carId);
+        return response()->json(new CarResource($car));
+    }
 }
