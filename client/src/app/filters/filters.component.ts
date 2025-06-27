71a7ebcabcdcbb0034c7e471bc5f44aefbc67e0a
Emma <graveemma842@gmail.com>
2025-06-27 13:28:10 +0200

    Updated filter for price with slider
    Ref : #74

--- client/src/app/filters/filters.component.ts
+++ client/src/app/filters/filters.component.ts
@@ -1,10 +1,11 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
 import {FormsModule} from '@angular/forms';
 import {NgForOf} from '@angular/common';
 import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
 import {MatFormField, MatInput, MatSuffix} from "@angular/material/input";
 import {MatLabel} from "@angular/material/form-field";
+import {NgxSliderModule, Options} from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-filters',
@@ -20,7 +21,8 @@ import {MatLabel} from "@angular/material/form-field";
     MatSuffix,
     MatInput,
     MatFormField,
-    MatDatepickerInput
+    MatDatepickerInput,
+    NgxSliderModule
   ],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
@@ -36,6 +38,8 @@ export class FiltersComponent {
   @Input() maxSelected: number = 0;
   @Input() departureDate: Date | null = null;
   @Input() returnDate: Date | null = null;
+  initialMinPrice: number = 0;
+  initialMaxPrice: number = 0;
 
   @Output() selected = new EventEmitter<{
     agencyId: string,
@@ -43,6 +47,24 @@ export class FiltersComponent {
     minSelected: number,
     maxSelected: number
   }>();
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ((changes['minPrice'] && changes['minPrice'].currentValue > 0) ||
+        (changes['maxPrice'] && changes['maxPrice'].currentValue > 0)) {
+
+      if (!this.initialMinPrice && this.minPrice > 0) {
+        this.initialMinPrice = this.minPrice;
+        this.minSelected = this.minPrice;
+      }
+
+      if (!this.initialMaxPrice && this.maxPrice > 0) {
+        this.initialMaxPrice = this.maxPrice;
+        this.maxSelected = this.maxPrice;
+      }
+
+      this.updateSliderOptions();
+    }
+  }
 
   onSelectionChange() {
     console.log(this.selectedCategoryId);
@@ -58,15 +80,33 @@ export class FiltersComponent {
   resetFilters() {
     this.selectedAgencyId = '';
     this.selectedCategoryId = '';
-    this.minSelected = this.minPrice;
-    this.maxSelected = this.maxPrice;
+    this.minSelected = this.initialMinPrice;
+    this.maxSelected = this.initialMaxPrice;
 
     this.selected.emit({
       agencyId: '',
       categoryId: '',
-      minSelected: 0,
-      maxSelected: 0
+      minSelected: this.initialMinPrice,
+      maxSelected: this.initialMaxPrice
     });
+  }
+
+  priceSliderOptions: Options = {
+    floor: this.minPrice,
+    ceil: this.maxPrice,
+    step: 0.1,
+    translate: (value: number): string => value + ' €'
+  };
+
+  updateSliderOptions() {
+    this.priceSliderOptions = {
+      ...this.priceSliderOptions,
+      floor: this.minPrice,
+      ceil: this.maxPrice
+    };
+
+    if (this.minSelected < this.minPrice) this.minSelected = this.minPrice;
+    if (this.maxSelected > this.maxPrice) this.maxSelected = this.maxPrice;
   }
 
 }
