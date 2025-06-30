6d7368634f887e23af39871b72a7394986b80d62
Emma <graveemma842@gmail.com>
2025-06-30 10:58:11 +0200

    List of locations for mobile
    ref : #25

--- mobile/src/app/profil/profil.component.ts
+++ mobile/src/app/profil/profil.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {IonAvatar, IonLabel} from "@ionic/angular/standalone";
+import {IonAvatar, IonItem, IonLabel} from "@ionic/angular/standalone";
 import {AuthService} from "../services/auth.service";
+import {LocationService} from "../services/location.service";
+import {Observable} from "rxjs";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-profil',
@@ -9,7 +12,9 @@ import {AuthService} from "../services/auth.service";
   standalone: true,
   imports: [
     IonAvatar,
-    IonLabel
+    IonLabel,
+    NgForOf,
+    IonItem
   ]
 })
 export class ProfilComponent  implements OnInit {
@@ -17,30 +22,47 @@ export class ProfilComponent  implements OnInit {
   name = '';
   fistname = '';
   email = '';
+  allLocations: any[] = [];
+  locationsForUser: any[] = [];
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private loc: LocationService) { }
 
   ngOnInit() {
-    this.getUser();
-  }
-
-  getUser() {
     this.auth.me().subscribe({
       next: (response: any) => {
-        console.log(response);
-
         const client = response.client;
-        console.log(client);
         this.id = client.id;
         this.name = client.name;
         this.fistname = client.firstname;
         this.email = client.email;
 
+        this.loc.getAllLocations().subscribe({
+          next: (data) => {
+            this.allLocations = data;
+            this.locationsForUser = this.getLocationsForUser(this.id);
+            console.log('📦 Locations pour ce client :', this.locationsForUser);
+          },
+          error: (err) => {
+            console.error('Erreur chargement locations :', err);
+          }
+        });
       },
       error: (err) => {
         console.error('Erreur lors de la récupération du profil', err);
       }
+    });
+  }
 
+  getLocationsForUser(id: number) {
+    return this.allLocations.filter(loc => loc.client?.id === id);
+  }
+
+  formatDate(dateStr: string): string {
+    const date = new Date(dateStr);
+    return date.toLocaleDateString('fr-FR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
     });
   }
 
