bc229de04d4e72f684c313c39a4e52b6d0447e9c
nyl_sauval <nyl_sauval@ens.univ-artois.fr>
2025-06-30 09:12:08 +0200

    Factory seeder retrait retour

--- server/database/factories/RetourFactory.php
+++ server/database/factories/RetourFactory.php
@@ -14,9 +14,11 @@ class RetourFactory extends Factory
     {
         return [
             'return_date' => $this->faker->date(),
-            'return_status_car' => $this->faker->randomElement(['available', 'maintenance', 'damaged']),
+            'return_interior_status_car' => $this->faker->randomElement(['Très bon', 'Bon', 'Moyen', 'Mauvais']),
+            'return_exterior_status_car' => $this->faker->randomElement(['Très bon', 'Bon', 'Moyen', 'Mauvais']),
             'return_mileage' => $this->faker->randomFloat(3, 0, 200000), // entre 0 et 200000 km avec 3 décimales
             'return_default' => $this->faker->optional()->sentence(),
+            'return_fuel_level' => $this->faker->numberBetween(0, 100),
             'user_id' => User::factory(),
             'location_id' => Location::factory(),
             'created_at' => now(),
