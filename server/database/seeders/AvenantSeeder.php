d90932cec14f91b5cae4a8a9a42c94e3289c644d
Emma <graveemma842@gmail.com>
2025-07-01 19:44:49 +0200

    Ajout d'avenants pour les locations qui ont eu des retours

--- server/database/seeders/AvenantSeeder.php
+++ server/database/seeders/AvenantSeeder.php
@@ -13,6 +13,18 @@ class AvenantSeeder extends Seeder
      */
     public function run(): void
     {
-        Avenant::factory()->count(10)->create();
+        Avenant::create([
+            'avenant_date' => '2025-07-01',
+            'avenant_details' => 'Extension de la durée de location',
+            'avenant_price' => 100.00,
+            'location_id' => 5,
+        ]);
+
+        Avenant::create([
+            'avenant_date' => '2025-07-02',
+            'avenant_details' => 'Ajout d\'une assurance supplémentaire',
+            'avenant_price' => 150.00,
+            'location_id' => 6,
+        ]);
     }
 }
