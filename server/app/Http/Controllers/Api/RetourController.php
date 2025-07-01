d5370cf4634361d6fc42bab46a58103b1e064b6a
Emma <graveemma842@gmail.com>
2025-07-01 15:45:50 +0200

    Retour form (back)
    ref : #21

--- server/app/Http/Controllers/Api/RetourController.php
+++ server/app/Http/Controllers/Api/RetourController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Retour;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 
@@ -21,6 +22,25 @@ class RetourController extends Controller
         }
 
         return response()->json($retour);
+    }
+
+    public function update(Request $request, Retour $retour) {
+        $validated = $request->validate([
+            'return_date' => 'required|date',
+            'return_mileage' => 'required|numeric',
+            'return_fuel_level' => 'required|integer',
+            'return_interior_status_car' => 'required|string',
+            'return_exterior_status_car' => 'required|string',
+            'return_default' => 'nullable|string',
+            'return_done' => 'required|boolean',
+        ]);
+
+        $retour->update($validated);
+
+        return response()->json([
+            'message' => 'Retrait mis à jour avec succès',
+            'data' => $retour
+        ]);
     }
 
 }
